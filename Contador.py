5fe7e9c5d644c011ac706adbb0847de0ae6fe7a2
Eduardo Arruda <eduardoarruda2001@outlook.com>
2024-01-13 20:30:02 -0300

    v2

--- Contador.py
+++ Contador.py
@@ -1,3 +1,6 @@
+print("Contade de letras")
 Name = input("Qual o seu nome?\n")
 length = len(Name)
-print("O nome " + Name + " tem " + str(length) + " letras")
+print("O nome " + Name + " tem " + str(length) + " letras!")
+
+enter = input("\nAperte Enter para fechar")
